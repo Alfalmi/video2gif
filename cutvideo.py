a005a0ca00a353bbcfbaa67f55c541f73799a9a4
pinooxd <pinooxd@gmail.com>
2021-05-18 20:37:05 -0700

    cl

--- cutvideo.py
+++ cutvideo.py
@@ -2,17 +2,17 @@
 from moviepy.editor import *
 
 # loading video dsa gfg intro video
-clip = VideoFileClip("WR-104.mp4")
+clip = VideoFileClip("video_2021-05-18_20-00-21.mp4")
 
 
 # getting only first 5 seconds
-clip = clip.subclip(347, 355)
+clip = clip.subclip(0, 5)
 
 # cutting out some part from the clip
 # clip = clip.cutout(3, 10)
 
 
-clip.write_gif("mygif.gif", fps=5)
+clip.write_gif("pinolargo.gif", fps=10)
 # showing  clip
 clip.ipython_display(width=120)
 
